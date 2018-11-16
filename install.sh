26612b35f8f8eaa058440fe5c5d8eeece6a8cfa2
Wagner Jeronimo <wagner@MacBook-Pro-de-Wagner.local>
2018-11-16 13:21:06 -0200

    install

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 echo "INICIANDO BUILD DO WS"
 #pull e build do mpr-ws
-git pull && mvn clean install
+git pull && mvn clean install -DskipTests=true
 
 path=target/ws.jar
 
 if [ -f "$path" ]; then
     echo "Removendo os containers e as imagens anteriores"
-    docker rm -f be && docker rmi mpr/ws
-    docker rm -f mysql && docker rmi mpr/mysql
+    docker rm -f ws && docker rmi mpr/ws
+    docker rm -f nginx && docker rmi mpr/nginx
     echo "Iniciando o compose"
     docker-compose up -d
     echo "FIM DO BUILD"
