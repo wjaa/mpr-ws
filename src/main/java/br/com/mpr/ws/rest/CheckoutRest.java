10d9197e5c1085c1d0f32fde92d28ebc392babc4
Wagner Jeronimo <wagner@MacBook-Pro-de-Wagner.local>
2019-01-17 22:30:17 -0200

    checkout rest

---
+++ src/main/java/br/com/mpr/ws/rest/CheckoutRest.java
@@ -0,0 +1,64 @@
+package br.com.mpr.ws.rest;
+
+
+import br.com.mpr.ws.entity.FreteType;
+import br.com.mpr.ws.exception.CarrinhoServiceException;
+import br.com.mpr.ws.exception.CheckoutServiceException;
+import br.com.mpr.ws.service.CarrinhoService;
+import br.com.mpr.ws.service.CheckoutService;
+import br.com.mpr.ws.vo.CarrinhoVo;
+import br.com.mpr.ws.vo.CheckoutVo;
+import br.com.mpr.ws.vo.ItemCarrinhoForm;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/api/v1/core")
+public class CheckoutRest extends BaseRest {
+
+    private static final Log LOG = LogFactory.getLog(CheckoutRest.class);
+
+
+    @Autowired
+    private CheckoutService checkoutService;
+
+
+    @RequestMapping(value = "/checkout/{idCarrinho}",
+            produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8",
+            method = RequestMethod.GET)
+    public CheckoutVo checkout(@PathVariable Long idCarrinho) throws CheckoutServiceException {
+        return this.checkoutService.checkout(idCarrinho);
+    }
+
+    @RequestMapping(value = "/checkout/{idCarrinho}/cupom",
+            produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8",
+            method = RequestMethod.POST)
+    public CheckoutVo adicionarCupom(@PathVariable Long idCarrinho,
+                               @PathVariable String cupom) throws CheckoutServiceException {
+        return this.checkoutService.adicionarCupom(idCarrinho,cupom);
+    }
+
+    @RequestMapping(value = "/checkout/{idCheckout}/{idEndereco}",
+            produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8",
+            method = RequestMethod.POST)
+    public CheckoutVo alterarEndereco(@PathVariable Long idCheckout,
+                                      @PathVariable Long idEndereco) throws CheckoutServiceException {
+        return this.checkoutService.alterarEndereco(idCheckout, idEndereco);
+    }
+
+    @RequestMapping(value = "/checkout/{idCheckout}/{freteType}",
+            produces = MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8",
+            method = RequestMethod.POST)
+    public CheckoutVo alterarFrete(@PathVariable Long idCheckout,
+                                      @PathVariable FreteType freteType) throws CheckoutServiceException {
+        return this.checkoutService.alterarFrete(idCheckout, freteType);
+    }
+}
---
+++ src/test/java/br/com/mpr/ws/rest/CheckoutRestTest.java
@@ -0,0 +1,23 @@
+package br.com.mpr.ws.rest;
+
+import br.com.mpr.ws.BaseMvcTest;
+import org.junit.Test;
+
+public class CheckoutRestTest extends BaseMvcTest {
+
+    @Test
+    public void checkout() {
+    }
+
+    @Test
+    public void adicionarCupom() {
+    }
+
+    @Test
+    public void alterarEndereco() {
+    }
+
+    @Test
+    public void alterarFrete() {
+    }
+}
