0034d4db8f5cf50ef034a9e3453229d4d8bf752c
Wagner Jeronimo <wag182@gmail.com>
2019-08-03 17:27:28 -0300

    NOVO INSTALL

---
+++ install_v2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "INICIANDO BUILD DO WS"
+echo "PARANDO OS CONTAINERS"
+docker stop ws && docker stop nginx
+
+#pull e build do mpr-ws
+git pull && mvn clean install
+
+path=target/ws.jar
+
+if [ -f "$path" ]; then
+    echo "Removendo os containers e as imagens anteriores"
+    docker rm -f ws && docker rmi mpr/ws
+    docker rm -f nginx && docker rmi mpr/nginx
+    echo "Iniciando o compose"
+    docker-compose up -d
+    echo "FIM DO BUILD"
+else
+    echo "ERROR: Algum erro no build não gerou o war. Implementar envio de email"
+    docker start ws && docker start nginx
+fi
